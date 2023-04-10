2a6fe9dbbc0f28056cf077ebc891b8216e50dbfd
ahamednadeem02 <111524475+ahamednadeem02@users.noreply.github.com>
2023-04-10 21:19:20 +0530

    final

--- main.py
+++ main.py
@@ -34,5 +34,13 @@ for file in filepaths:
         pdf.cell(w=32, h=8, txt=str(row['amount_purchased']), border=1)
         pdf.cell(w=30, h=8, txt=str(row['price_per_unit']), border=1)
         pdf.cell(w=30, h=8, txt=str(row['total_price']), border=1, ln=1)
+    total_sum = df['total_price'].sum()
+    pdf.set_font(family="Times", size=10)
+    pdf.set_text_color(80, 80, 80)
+    pdf.cell(w=30, h=8, txt=" ", border=1)
+    pdf.cell(w=70, h=8, txt=" ", border=1)
+    pdf.cell(w=32, h=8, txt=" ", border=1)
+    pdf.cell(w=30, h=8, txt=" ", border=1)
+    pdf.cell(w=30, h=8, txt=str(total_sum), border=1, ln=1)
 
     pdf.output(f"PDFs/{filename}.pdf")
